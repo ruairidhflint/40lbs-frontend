2c33d8623e02a4166113f9820c4003e624be418b
Rory Flint <roryflintphoto@gmail.com>
2020-02-11 23:21:33 +0000

    Add styling and homepage color testing"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/globalStyles';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${props => props.theme.colorTwo};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    height: 85%;
+    background: ${props => props.theme.colorTwo};
+    div {
+      padding-bottom: 0.5rem;
+      border-bottom: 1px solid ${props => props.theme.colorOne};
+      h1 {
+        font-size: 3rem;
+        letter-spacing: 0.3rem;
+        color: ${props => props.theme.colorFour};
+      }
+    }
+  }
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        <h1>forty pounds</h1>
-      </div>
+      <StyledContainer>
+        <div className="container">
+          <div>
+            <h1>forty pounds</h1>
+          </div>
+        </div>
+      </StyledContainer>
     </ThemeProvider>
   );
 }
