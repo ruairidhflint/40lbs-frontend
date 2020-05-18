889ceee0d2ba48cf0a9afe8f809f0840cf67b1f2
Rory Flint <roryflintphoto@gmail.com>
2020-05-18 16:46:57 +0100

    Import landing page to main app body

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+// View Imports
+import LandingPage from './Views/Landing';
 
 function App() {
-  return <div className="AppContainer"></div>;
+  return (
+  <div className="AppContainer">
+    <Route exact path="/" component={LandingPage} />
+  </div>
+  );
 }
 
 export default App;
