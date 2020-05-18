027c91e74e4f3cfc9d5466fb12e95c8554f7e2e2
Rory Flint <roryflintphoto@gmail.com>
2020-05-18 15:31:55 +0100

    Strip out all old data and start fresh

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import GlobalStyle from './styles/globalStyles';
-import TestGraph from './Components/testgraph';
-import Landing from './Components/Landing';
-
-const StyledContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .nav {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: baseline;
-
-    h1 {
-      width: 250px;
-      text-align: center;
-      font-size: 2rem;
-      letter-spacing: 0.3rem;
-      margin-top: 1rem;
-      color: ${props => props.theme.colorFour};
-      padding-bottom: 0.5rem;
-      margin-left: 2rem;
-      border-bottom: 1px solid ${props => props.theme.colorOne};
-    }
-
-    h3 {
-      color: ${props => props.theme.colorFour};
-      font-size: 1.2rem;
-      margin-right: 4rem;
-    }
-  }
-`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <StyledContainer>
-        <div className="nav">
-          <h1>forty pounds</h1>
-          <h3>Log in</h3>
-        </div>
-        <Landing />
-      </StyledContainer>
+      <div className="App">
+        Hello, world.
+      </div>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-/* 
-TODO -
-- Settle on color scheme
-- Design Landing Page
-- Design Login/Signup Page (Dribble)
-- Design display page
--Design Upload Modal
-
-*/
