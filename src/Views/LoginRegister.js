61030ea692b1dc3608f9fb832254ae70d26c1427
Rory Flint <roryflintphoto@gmail.com>
2020-05-19 15:49:43 +0100

    Major structure to register card

--- src/Views/LoginRegister.js
+++ src/Views/LoginRegister.js
@@ -1,22 +1,185 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
-
 function LoginRegister() {
+  const [login, setLogin] = useState(false);
+
+  const switchAuth = () => {
+    setLogin(!login);
+  };
+
   return (
     <StyledLoginContainer>
-      
+      <div className="container">
+        <div className="title">
+          <Link to="/">
+            <h3>forty pounds</h3>
+          </Link>
+        </div>
+        <div className="main">
+          <h4>Create your account</h4>
+          <span onClick={switchAuth}>Already registered?</span>
+          <form>
+            <label>Email</label>
+            <input type="email" required />
+            <label> Password</label>
+            <input type="password" required />
+            <label> Confirm Password</label>
+            <input type="password" required />
+            <label> Current Weight (lbs)</label>
+            <input type="number" required />
+            <p>
+              By registering, you aree to Forty Pound's{' '}
+              <Link to="/">Terms of Conditions</Link> and{' '}
+              <Link to="/">Privacy Policy</Link>
+            </p>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </div>
     </StyledLoginContainer>
-    );
+  );
 }
 
 const StyledLoginContainer = styled.main`
   width: 100vw;
-  height: 90vh;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  background-color: ${(props) => props.theme.background};
+
+  .container {
+    width: 35%;
+    height: 73%;
+    box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.07);
+    border-radius: 4px;
+    background-color: ${(props) => props.theme.white};
+    display: flex;
+    flex-direction: column;
+    color: ${(props) => props.theme.mainText};
+
+    .title {
+      width: 100%;
+      height: 8%;
+      background-color: ${(props) => props.theme.white};
+      border-radius: 8px 8px 0 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      a {
+        text-decoration: none;
+        transition: opacity 0.3s ease-in-out;
+        color: inherit;
+
+        :hover {
+          opacity: 0.8;
+          transition: opacity 0.3s ease-in-out;
+        }
+      }
+
+      h3 {
+        font-size: 1.1rem;
+        opacity: 0.8;
+      }
+    }
+
+    .main {
+      width: 100%;
+      height: 92%;
+      background-color: ${(props) => props.theme.white};
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      border-radius: 0 0 8px 8px;
+
+      h4 {
+        font-size: 1.8rem;
+        font-weight: bold;
+        margin: 0.5rem 0 0.5rem 0;
+      }
+
+      span {
+        font-size: 0.7rem;
+        margin-bottom: 1rem;
+        cursor: pointer;
+        transition: color 0.2s ease-in-out;
+        :hover {
+          color: ${(props) => props.theme.green};
+          transition: color 0.2s ease-in-out;
+        }
+      }
+
+      p {
+        width: 70%;
+        opacity: 0.7;
+        font-size: 0.8rem;
+        line-height: 1.2;
+        text-align: center;
+        margin-top: 0.5rem;
+
+        a {
+          text-decoration: none;
+          color: inherit;
+          font-weight: bold;
+          transition: color 0.2s ease-in-out;
+          :hover {
+            color: ${(props) => props.theme.yellow};
+            transition: color 0.2s ease-in-out;
+          }
+        }
+      }
+      form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+      label {
+        width: 80%;
+        margin-bottom: 0.4rem;
+        font-size: 0.9rem;
+        opacity: 0.8;
+      }
+      input {
+        display: block;
+        width: 80%;
+        margin-bottom: 0.7rem;
+        height: 2.7rem;
+        border: none;
+        border-radius: 4px;
+        background-color: ${(props) => props.theme.backgroundSecondary};
+        font-size: 1.2rem;
+        padding-left: 1rem;
+        border: 2px solid ${(props) => props.theme.white};
+        border-radius: 4px;
+
+        :focus {
+          outline: none;
+          border: 2px solid ${(props) => props.theme.mainBlue};
+          border-radius: 4px;
+        }
+
+        :hover {
+          background-color: ${(props) => props.theme.background};
+        }
+      }
+
+      button {
+        width: 70%;
+        height: 40px;
+        border: none;
+        background-color: ${(props) => props.theme.red};
+        border-radius: 5px;
+        color: ${(props) => props.theme.white};
+        font-size: 1.2rem;
+        margin-top: 1.2rem;
+      }
+    }
+  }
+`;
 
 export default LoginRegister;
