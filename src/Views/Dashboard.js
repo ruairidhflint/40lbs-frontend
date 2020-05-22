b3f9a8f823f0be903470d3533c61f20d54558184
Rory Flint <roryflintphoto@gmail.com>
2020-05-22 09:52:38 +0100

    Build cursory API call to retrieve user info

--- src/Views/Dashboard.js
+++ src/Views/Dashboard.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Dashboard(props) {
-  console.log(props);
+import { axiosWithBase } from '../Helpers/axios';
+
+function Dashboard() {
+  const [data, setData] = useState({currentWeight:"", startWeight:"", weights: []});
+  const fetchData = () => {
+    axiosWithBase
+      .get('weight/user/all')
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <h1 style={{fontSize: '22vw'}}>dashboard</h1>
-    );
+    <>
+      <h1 style={{ fontSize: '22vw' }}>dashboard</h1>
+      <h2>Current Weight: {data.currentWeight}</h2>
+      <h2>Start Weight: {data.startWeight}</h2>
+      {data.weights.map((weight) => {
+        return <p>{weight.current_weight}</p>;
+      })}
+    </>
+  );
 }
 
 export default Dashboard;
