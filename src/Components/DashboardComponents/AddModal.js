7a178df90e7813776bc4c2e9c7fb7dad92be955f
Rory Flint <roryflintphoto@gmail.com>
2020-05-25 14:25:42 +0100

    Major styling added to modal

--- src/Components/DashboardComponents/AddModal.js
+++ src/Components/DashboardComponents/AddModal.js
@@ -22,8 +22,10 @@ function AddModal(props) {
             <input type="number" step="0.1" name="new-weight" required />
           </div>
           <div className="buttons">
-            <button>Cancel</button>
-            <button>Submit</button>
+            <button onClick={closeModal} className="cancel">
+              Cancel
+            </button>
+            <button className="submit">Submit</button>
           </div>
         </form>
       </div>
@@ -32,95 +34,123 @@ function AddModal(props) {
 }
 
 const StyledAddModal = styled.div`
-.container {
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 5, 0.5);
-  position: absolute;
-  top: 0px;
-  z-index: 50;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  display: ${(props) => (props.visible ? 'display' : 'none')};
-
-  .modal {
-    width: 40vw;
-    height: 30vh;
-    background-color: ${(props) => props.theme.white};
-    border-radius: 3px;
+  .container {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 5, 0.5);
+    position: absolute;
+    top: 0px;
+    z-index: 50;
     display: flex;
-    flex-direction: column;
+    justify-content: center;
     align-items: center;
+    display: ${(props) => (props.visible ? 'display' : 'none')};
 
-    .title {
-      width: 100%;
-      height: 20%;
+    .modal {
+      width: 40vw;
+      height: 30vh;
+      background-color: ${(props) => props.theme.white};
+      border-radius: 3px;
       display: flex;
-      justify-content: center;
+      flex-direction: column;
       align-items: center;
 
-      h3 {
-        font-size: 1.6rem;
+      .title {
+        width: 100%;
+        height: 25%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        h3 {
+          font-size: 1.6rem;
+        }
       }
-    }
 
-    .input {
-      width: 100%;
-      height: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+      .input {
+        width: 100%;
+        height: 42%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-      input {
-        display: block;
-        width: 90%;
-        margin-bottom: 0.7rem;
-        height: 3em;
-        border: none;
-        border-radius: 4px;
-        background-color: ${(props) => props.theme.background};
-        font-size: 1.2rem;
-        padding-left: 1rem;
-        border: 2px solid ${(props) => props.theme.white};
-        border-radius: 4px;
-        transition: background-color 0.3s ease-in-out;
-
-        ::placeholder {
-          font-size: 0.9rem;
-          color: ${(props) => props.theme.red};
-        }
-
-        @media (max-width: 400px) {
-          width: 100%;
-        }
-
-        :focus {
-          outline: none;
-          border: 2px solid ${(props) => props.theme.mainBlue};
+        input {
+          display: block;
+          width: 90%;
+          height: 3em;
+          border: none;
           border-radius: 4px;
-        }
-
-        // :valid {
-        //   border: 2px solid ${(props) => props.theme.green};
-        // }
-
-        :hover {
-          background-color: ${(props) => props.theme.backgroundSecondary};
+          background-color: ${(props) => props.theme.background};
+          font-size: 1.2rem;
+          padding-left: 1rem;
+          border: 2px solid ${(props) => props.theme.white};
+          border-radius: 4px;
           transition: background-color 0.3s ease-in-out;
+
+          ::placeholder {
+            font-size: 0.9rem;
+            color: ${(props) => props.theme.red};
+          }
+
+          @media (max-width: 400px) {
+            width: 100%;
+          }
+
+          :focus {
+            outline: none;
+            border: 2px solid ${(props) => props.theme.mainBlue};
+            border-radius: 4px;
+          }
+
+          :hover {
+            background-color: ${(props) => props.theme.backgroundSecondary};
+            transition: background-color 0.3s ease-in-out;
+          }
         }
       }
-    }
 
-    .buttons {
-      width: 95%;
-      height: 30%;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
+      .buttons {
+        width: 92%;
+        height: 33%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        button {
+          border: none;
+          height: 2.5rem;
+          width: 7rem;
+          margin-left: 0.7rem;
+          cursor: pointer;
+          border-radius: 4px;
+          font-size: 1rem;
+          color: ${(props) => props.theme.white};
+
+          &.cancel {
+            background-color: none;
+            color: ${(props) => props.theme.mainText};
+            transition: color 0.2s ease-in-out;
+
+            :hover {
+              color: ${(props) => props.theme.red};
+              transition: color 0.2s ease-in-out;
+            }
+          }
+
+          &.submit {
+            background-color: ${(props) => props.theme.green};
+            transition: all 0.2s ease-in-out;
+
+            :hover {
+              opacity: 0.8;
+              transform: scale(1.05);
+              transition: all 0.2s ease-in-out;
+            }
+          }
+        }
+      }
     }
   }
-}
 `;
 
 export default AddModal;
